b3d0d2155ed5b656ec44cb863bb3dec9de31704b
Victor Trindade <victortrindade@gmail.com>
2024-03-07 10:57:04 -0300

    Fix height of chart

--- src/pages/Chart/styles.js
+++ src/pages/Chart/styles.js
@@ -4,12 +4,12 @@ export const Container = styled.div`
   display: flex;
   justify-content: center;
   /* align-items: center; */
-  height: 88vh;
+  /* height: 88vh; */
 `;
 
 export const TradingViewContainer = styled.div`
   margin-top: 5vh;
-  height: 70vh;
+  /* height: 70vh; */
   width: 90%;
 
   border: #333333 solid 2px;
